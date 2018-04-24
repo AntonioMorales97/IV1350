1bd83cc1ac602b337757248ad898c614835e6cdc
Antonio <antonio_97@live.se>
2018-04-24 21:33:29 +0200

    init commit

--- Test Packages/se/kth/iv1350/processofsale/controller/ControllerTest.java
+++ Test Packages/se/kth/iv1350/processofsale/controller/ControllerTest.java
@@ -1,14 +1,54 @@
 package se.kth.iv1350.processofsale.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
-class ControllerTest {
+import se.kth.iv1350.processofsale.integration.ItemDTO;
+import se.kth.iv1350.processofsale.model.CurrentInfo;
+import se.kth.iv1350.processofsale.model.Sale;
+
+public class ControllerTest {
+
+	private Controller controller;
+
+	@Before
+	public void setUp() {
+		this.controller = new Controller();
+		this.controller.startNewSale();
+
+	}
+
+	@After
+	public void tearDown() {
+		this.controller = null;
+	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testStartNewSale() {
+		Sale expResult = this.controller.getSale();
+		assertNotNull("Sale is null.", expResult);
+
+	}
+
+	@Test
+	public void testEnterItem() {
+		CurrentInfo currentInfo = this.controller.enterItem(1);
+		assertNotNull("CurrentInfo is null.");
+		ItemDTO itemDTO = currentInfo.getItemDTO();
+		assertNotNull("ItemDTO is null.", itemDTO);
+		boolean expRunningTotal = currentInfo.getRunningTotal() > 0;
+		assertTrue("Running total less than or equal to zero.", expRunningTotal);
+	}
+
+	@Test
+	public void testEnterItems() {
+		CurrentInfo currentInfo = this.controller.enterItems(1, 10);
+		assertNotNull("CurrentInfo is null.", currentInfo);
 	}
 
 }
