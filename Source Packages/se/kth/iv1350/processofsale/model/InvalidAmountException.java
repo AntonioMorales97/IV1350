4896e1d543069dd8f5194934216c3bf955e0d770
Antonio <antonio_97@live.se>
2018-05-02 15:25:49 +0200

    added description

--- Source Packages/se/kth/iv1350/processofsale/model/InvalidAmountException.java
+++ Source Packages/se/kth/iv1350/processofsale/model/InvalidAmountException.java
@@ -1,5 +1,8 @@
 package se.kth.iv1350.processofsale.model;
 
+/**
+ * This exception is thrown whenever the amount is still below the total cost that needs to be paid. 
+ */
 public class InvalidAmountException extends Exception {
 	InvalidAmountException(String msg, double amount) {
 		super(msg + String.format("%.2f", amount));
--- Source Packages/se/kth/iv1350/processofsale/model/InvalidIdentifierException.java
+++ Source Packages/se/kth/iv1350/processofsale/model/InvalidIdentifierException.java
@@ -1,5 +1,8 @@
 package se.kth.iv1350.processofsale.model;
 
+/**
+ * This exception is thrown whenever an invalid item identifier or customer identifier is detected.
+ */
 public class InvalidIdentifierException extends Exception {
 	
 	public InvalidIdentifierException(String msg) {
