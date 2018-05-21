2b077e42986eabc728fdfbb98d43c2368eca13fa
Antonio <antonio_97@live.se>
2018-05-21 16:21:53 +0200

    relocated to util package

--- Source Packages/se/kth/iv1350/processofsale/model/Logger.java
+++
@@ -1,8 +0,0 @@
-package se.kth.iv1350.processofsale.model;
-
-/**
- * An interface for classes that needs to log something.
- */
-public interface Logger {
-	public void logException(Exception exc);
-}
